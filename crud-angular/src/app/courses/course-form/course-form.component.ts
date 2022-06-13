418d1af248501baa466afd2ce665735526a69f26
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-06-13 09:34:31 -0300

    Add form to new course with buttons

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [''],
+      category: ['']
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  onCancel() {
+    this.form.reset();
   }
 
 }
