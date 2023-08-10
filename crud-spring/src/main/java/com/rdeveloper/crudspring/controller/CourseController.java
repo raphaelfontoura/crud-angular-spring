91ee68e3d267d050136365397f77d2b2c074b132
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-08-10 00:47:00 -0300

    Upgrade to spring boot 3.1. Change @Length to @Size -> jakarta.validation

--- crud-spring/src/main/java/com/rdeveloper/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/rdeveloper/crudspring/controller/CourseController.java
@@ -2,9 +2,9 @@ package com.rdeveloper.crudspring.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- crud-spring/src/main/java/com/rdeveloper/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/rdeveloper/crudspring/model/Course.java
@@ -1,17 +1,17 @@
 package com.rdeveloper.crudspring.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -29,18 +29,18 @@ public class Course {
   private Long id;
 
   @NotBlank
-  @Length(min = 5, max = 100)
+  @Size(min = 5, max = 100)
   @Column(length = 100, nullable = false)
   private String name;
 
   @NotNull
-  @Length(max = 10)
+  @Size(max = 10)
   @Pattern(regexp = "back-end|front-end")
   @Column(length = 20, nullable = false)
   private String category;
 
   @NotNull
-  @Length(max = 10)
+  @Size(max = 10)
   @Pattern(regexp = "Active|Inactive")
   @Column(length = 10, nullable = false)
   private String status = "Active";
