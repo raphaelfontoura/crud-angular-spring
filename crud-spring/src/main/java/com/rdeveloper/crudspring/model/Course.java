4b98fd72a61aacd84bf589eebcbd2f13ec3d2438
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-08-10 00:37:01 -0300

    Add softdelete to Course with hibernate annotations

--- crud-spring/src/main/java/com/rdeveloper/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/rdeveloper/crudspring/model/Course.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -17,6 +19,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?")
+@Where(clause = "status = 'Active'")
 public class Course {
   
   @Id
@@ -34,4 +38,10 @@ public class Course {
   @Pattern(regexp = "back-end|front-end")
   @Column(length = 20, nullable = false)
   private String category;
+
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Active|Inactive")
+  @Column(length = 10, nullable = false)
+  private String status = "Active";
 }
