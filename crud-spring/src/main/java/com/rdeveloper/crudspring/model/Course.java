811ae91484a002582041b6bdd1e82cab0f4fa1bc
Raphael Fontoura <raphael.fontoura@gmail.com>
2025-04-17 00:30:02 -0300

    Add Status enum and StatusConverter. Update Course model to use Status enum.

--- crud-spring/src/main/java/com/rdeveloper/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/rdeveloper/crudspring/model/Course.java
@@ -1,25 +1,23 @@
 package com.rdeveloper.crudspring.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.rdeveloper.crudspring.enums.Category;
+import com.rdeveloper.crudspring.enums.Status;
 import com.rdeveloper.crudspring.enums.converters.CategoryConverter;
+import com.rdeveloper.crudspring.enums.converters.StatusConverter;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -27,25 +25,24 @@ import lombok.Data;
 @SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?")
 @Where(clause = "status = 'Active'")
 public class Course {
-  
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @JsonProperty("_id")
-  private Long id;
 
-  @NotBlank
-  @Size(min = 5, max = 100)
-  @Column(length = 100, nullable = false)
-  private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
+    private Long id;
 
-  @NotNull
-  @Column(length = 20, nullable = false)
-  @Convert(converter = CategoryConverter.class)
-  private Category category;
+    @NotBlank
+    @Size(min = 5, max = 100)
+    @Column(length = 100, nullable = false)
+    private String name;
 
-  @NotNull
-  @Size(max = 10)
-  @Pattern(regexp = "Active|Inactive")
-  @Column(length = 10, nullable = false)
-  private String status = "Active";
+    @NotNull
+    @Column(length = 20, nullable = false)
+    @Convert(converter = CategoryConverter.class)
+    private Category category;
+
+    @NotNull
+    @Column(length = 10, nullable = false)
+    @Convert(converter = StatusConverter.class)
+    private Status status = Status.ACTIVE;
 }
