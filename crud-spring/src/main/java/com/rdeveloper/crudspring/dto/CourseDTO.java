3d42daeaa8851e961c862a1de9515313e72a68f4
Raphael Fontoura <raphael.fontoura@gmail.com>
2025-07-10 21:58:51 -0300

    Add ValueOfEnum annotation and validator for category validation in CourseDTO

--- crud-spring/src/main/java/com/rdeveloper/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/rdeveloper/crudspring/dto/CourseDTO.java
@@ -3,12 +3,13 @@ package com.rdeveloper.crudspring.dto;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.rdeveloper.crudspring.enums.Category;
+import com.rdeveloper.crudspring.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CourseDTO(
@@ -16,7 +17,7 @@ public record CourseDTO(
 
         @NotBlank @NotNull @Size(min = 5, max = 100) String name,
 
-        @NotNull @Size(max = 10) @Pattern(regexp = "back-end|front-end") String category,
+        @NotNull @ValueOfEnum(enumClass = Category.class) String category,
         
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 
