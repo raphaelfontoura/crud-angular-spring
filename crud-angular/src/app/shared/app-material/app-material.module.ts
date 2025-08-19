07e5e9c58668e91cb8c1b3472da37cc6be611339
Raphael Fontoura <raphael.fontoura@gmail.com>
2025-08-18 21:54:12 -0300

    Add pagination to courses list and update service to support paginated data

--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -10,7 +10,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 @NgModule({
@@ -25,7 +25,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatPaginatorModule
   ],
 })
 export class AppMaterialModule { }
